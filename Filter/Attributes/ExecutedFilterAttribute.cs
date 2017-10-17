947b679c1e4bca0bad274dd15d202ffa1e728887
waning1995 <a937515012@live.com>
2017-10-17 08:59:57 +0800

    修改-更新注解, 删除对属性的支持

--- Filter/Attributes/ExecutedFilterAttribute.cs
+++ Filter/Attributes/ExecutedFilterAttribute.cs
@@ -5,12 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using Filter.Model;
 
-namespace Filter.Attributs
+namespace Filter.Attributes
 {
     /// <summary>
     /// 方法执行结束过滤器
     /// </summary>
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public abstract class ExecutedFilterAttribute : FilterAttribute
     {
         /// <summary>
